5e6eef7ac30cd3f79563942ecaa5ec8ebf286471
santanu12saha <saha.santanu0217@gmail.com>
2020-04-19 18:19:02 +0530

    Adding third party module loadash.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,6 +1,15 @@
 console.log('Staring app.');
 
 const fs = require('fs');
+const _ = require('lodash');
+
+//https://lodash.com/docs/#isString
+console.log(_.isString(true));
+console.log(_.isString('Santanu'));
+
+// https://lodash.com/docs/#uniq
+var filteredArray = _.uniq(['Santanu', 1, 'Santanu', 1, 2, 3, 4]);
+console.log(filteredArray);
 
 fs.appendFile('greetings.txt', 'Hello world!', err => {
     if(err){
