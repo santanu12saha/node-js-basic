ed8b9a44bfec69595d5cc07595ac61ae35ca3f2b
santanu12saha <saha.santanu0217@gmail.com>
2020-04-26 20:36:54 +0530

    Add node debug and repl to do debugging in cmd.

--- note-app/notes.js
+++ note-app/notes.js
@@ -38,8 +38,10 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Getting note', title);
-}
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
+};
 
 var removeNote = (title) => {
     //fetch Notes.
@@ -50,11 +52,19 @@ var removeNote = (title) => {
     saveNotes(filteredNotes);
 
     return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+    debugger;
+    console.log('----');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 }
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
---
+++ note-app/playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'Santanu'
+};
+
+person.age = 30;
+
+debugger;
+
+person.name = 'Tapasmita';
+
+console.log(person);
