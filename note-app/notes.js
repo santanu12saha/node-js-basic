c8049295428bf869850c77b2bd4517f14e11f13b
santanu12saha <saha.santanu0217@gmail.com>
2020-04-26 20:47:11 +0530

    Add fetch notes functionality.

--- note-app/notes.js
+++ note-app/notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 const saveNoteFileName = 'notes-data.json';
 
@@ -34,7 +32,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-    console.log("Getting all notes");
+    return fetchNotes();
 };
 
 var getNote = (title) => {
