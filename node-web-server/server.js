fb5ac83e7c592d89042ed20f412f5df0a33766d5
santanu12saha <saha.santanu0217@gmail.com>
2020-05-05 22:54:46 +0530

    Adding Json Response.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,24 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Santanu',
+        likes: [
+            'Painting',
+            'Cities',
+            'Music'
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
 });
 
 app.listen(3030);
