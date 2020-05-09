381b91ad85d248d2e435b9bcc100a104a96bc788
santanu12saha <saha.santanu0217@gmail.com>
2020-05-09 11:03:24 +0530

    Render static web file from express.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.send({
         name: 'Santanu',
@@ -23,4 +25,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3030);
+app.listen(3030, () => {
+    console.log('Server is up on port 3030');
+});
