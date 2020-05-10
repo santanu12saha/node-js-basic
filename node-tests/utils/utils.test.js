c13cf15a1771dd7ea8aac9e0e28e525bc1dd4b71
santanu12saha <saha.santanu0217@gmail.com>
2020-05-10 20:35:00 +0530

    Add test case using expect or jest library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,16 +1,50 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if(res !== 44){
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
-    
+    expect(res).toBe(44);
+    expect(typeof res).toBe('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(3);
-    if(res !== 9){
-        throw new Error(`Expected 9, but got ${res}.`);
-    }
+    expect(res).toBe(9);
+    expect(typeof res).toBe('number');
+});
+
+it('should expect same value', () => {
+    expect(12).toBe(12);
+});
+
+it('should expect same object', () => {
+    expect({name: 'Santanu'}).toEqual({name: 'Santanu'});
+});
+
+it('should check element inside array', () => {
+    expect([1,2,3]).toContain(1);
+});
+
+it('should check particular property from object', () => {
+    expect({
+        name: 'Santanu',
+        age: 29
+    }).toHaveProperty('age');
+});
+
+it('should check property and value from object', () => {
+    expect({
+        name: 'Santanu',
+        age: 29
+    }).toMatchObject({'age': 29});
+});
+
+it('should set firstName and lastName', () => {
+    var user = {location: 'India', age: 30};
+    var res = utils.setName(user, 'Santanu Saha');
+    
+    expect(res).toEqual(user);
+    expect(res).toMatchObject({firstName: 'Santanu', lastName: 'Saha'});
+    expect(res).toHaveProperty('firstName', 'Santanu');
 });
