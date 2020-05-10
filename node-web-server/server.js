59d5d079c176cba68137a67a7c97c1dd9b0fbbdc
santanu12saha <saha.santanu0217@gmail.com>
2020-05-10 15:49:51 +0530

    Add scripts and dynamic port configuration.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3030;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -63,6 +64,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3030, () => {
-    console.log('Server is up on port 3030');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
