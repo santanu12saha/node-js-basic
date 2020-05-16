5825a627130b3abb7ab5f4f8147109dc05c23df5
santanu12saha <saha.santanu0217@gmail.com>
2020-05-16 13:06:47 +0530

    Add grouping of test case using describe.

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,37 +3,43 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-     .get('/')
-     .expect(200)
-     .expect('Hello World!')
-     .end(done);
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+             .get('/')
+             .expect(200)
+             .expect('Hello World!')
+             .end(done);
+        });
+    });
+
+    describe('GET /status', () => {
+        it('should check response status code', (done) => {
+            request(app)
+             .get('/status')
+             .expect(404)
+             .expect((res) => {
+                expect(res.body).toMatchObject({
+                    error: 'Page not found.'
+                })
+             })
+             .end(done);
+        });
+    });
+
+    describe('GET /users', () => {
+        it('should return user objects', (done) => {
+            request(app)
+             .get('/users')
+             .expect(200)
+             .expect((res) => {
+                 expect(res.body).toEqual(expect.arrayContaining([{
+                     name: 'Santanu Saha',
+                     age: 30
+                 }]));
+             })
+             .end(done);
+        });
+    });
 });
-
-it('should check response status code', (done) => {
-    request(app)
-     .get('/status')
-     .expect(404)
-     .expect((res) => {
-        expect(res.body).toMatchObject({
-            error: 'Page not found.'
-        })
-     })
-     .end(done);
-});
-
-it('should return user objects', (done) => {
-    request(app)
-     .get('/users')
-     .expect(200)
-     .expect((res) => {
-         expect(res.body).toEqual(expect.arrayContaining([{
-             name: 'Santanu Saha',
-             age: 30
-         }]));
-     })
-     .end(done);
-});
-
-
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,66 +2,84 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
-    expect(res).toBe(44);
-    expect(typeof res).toBe('number');
-});
+describe('Utils', () => {
 
-it('should async add two number', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7);
-        expect(typeof sum).toBe('number');
-        done();
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+            expect(res).toBe(44);
+            expect(typeof res).toBe('number');
+        });
+        
+        it('should async add two number', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7);
+                expect(typeof sum).toBe('number');
+                done();
+            });
+        });
     });
-});
-
-it('should square a number', () => {
-    var res = utils.square(3);
-    expect(res).toBe(9);
-    expect(typeof res).toBe('number');
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(5, (square) => {
-        expect(square).toBe(25);
-        expect(typeof square).toBe('number');
-        done();
-    });
-});
-
-
-it('should expect same value', () => {
-    expect(12).toBe(12);
-});
-
-it('should expect same object', () => {
-    expect({name: 'Santanu'}).toEqual({name: 'Santanu'});
-});
-
-it('should check element inside array', () => {
-    expect([1,2,3]).toContain(1);
-});
-
-it('should check particular property from object', () => {
-    expect({
-        name: 'Santanu',
-        age: 29
-    }).toHaveProperty('age');
-});
-
-it('should check property and value from object', () => {
-    expect({
-        name: 'Santanu',
-        age: 29
-    }).toMatchObject({'age': 29});
-});
-
-it('should set firstName and lastName', () => {
-    var user = {location: 'India', age: 30};
-    var res = utils.setName(user, 'Santanu Saha');
     
-    expect(res).toEqual(user);
-    expect(res).toMatchObject({firstName: 'Santanu', lastName: 'Saha'});
-    expect(res).toHaveProperty('firstName', 'Santanu');
+    describe('#square', () => {
+        it('should square a number', () => {
+            var res = utils.square(3);
+            expect(res).toBe(9);
+            expect(typeof res).toBe('number');
+        });
+        
+        it('should async square a number', (done) => {
+            utils.asyncSquare(5, (square) => {
+                expect(square).toBe(25);
+                expect(typeof square).toBe('number');
+                done();
+            });
+        });
+    
+    });
+
+    describe("#userSetName", () => {
+        it('should set firstName and lastName', () => {
+            var user = {location: 'India', age: 30};
+            var res = utils.setName(user, 'Santanu Saha');
+            
+            expect(res).toEqual(user);
+            expect(res).toMatchObject({firstName: 'Santanu', lastName: 'Saha'});
+            expect(res).toHaveProperty('firstName', 'Santanu');
+        });
+    });
+
+
 });
+
+describe("General", () => {
+    it('should expect same value', () => {
+        expect(12).toBe(12);
+    });
+    
+    it('should expect same object', () => {
+        expect({name: 'Santanu'}).toEqual({name: 'Santanu'});
+    });
+    
+    it('should check element inside array', () => {
+        expect([1,2,3]).toContain(1);
+    });
+    
+    it('should check particular property from object', () => {
+        expect({
+            name: 'Santanu',
+            age: 29
+        }).toHaveProperty('age');
+    });
+    
+    it('should check property and value from object', () => {
+        expect({
+            name: 'Santanu',
+            age: 29
+        }).toMatchObject({'age': 29});
+    });
+});
+
+
+
+
+
